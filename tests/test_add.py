69a37a304effb8c7fd6d31410c6f2dd8ce7dc659
reedlauliu <reedlauliu@tencent.com>
2025-06-25 20:49:35 +0800

    update relative path

--- tests/test_add.py
+++ tests/test_add.py
@@ -1,6 +1,10 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../build/lib.linux-x86_64-cpython-310/')))
+
 import hpc
 import torch
-
 
 def test_add():
   a = torch.randn(3, 5, device='cuda')
