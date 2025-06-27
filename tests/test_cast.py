bab23a71b0847c8d3c4b2494f6f7a4070b35a885
reedlauliu <reedlauliu@tencent.com>
2025-06-27 11:32:51 +0800

    fix test work dir

--- tests/test_cast.py
+++ tests/test_cast.py
@@ -2,8 +2,8 @@ import sys
 import os
 from pathlib import Path
 
-sys.path.append(
-    os.path.realpath(list(Path(__file__).parent.glob('../build/lib.*/'))[0]))
+sys.path.insert(
+    0, os.path.realpath(list(Path(__file__).parent.glob('../build/lib.*/'))[0]))
 
 import hpc
 import torch
