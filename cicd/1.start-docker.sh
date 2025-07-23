cb85404362f75d1ce25e83bb11ce01b44168a1b7
reedlauliu <reedlauliu@tencent.com>
2025-07-23 14:43:14 +0800

    move docker image to pipeline

--- cicd/1.start-docker.sh
+++ cicd/1.start-docker.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
 set -xeo pipefail
 
-BUILD_ID=$1
+pwd
 
-DOCKER_IMAGE=mirrors.tencent.com/hunyuan_infer/text2text_infer:H-5.0.1-v7-hpc-ops
-DATE=$(date +"%Y_%m_%d")
+ls -lthr
 
-CONTAINER_NAME=hpc-builder-$BUILD_ID
-
-sudo docker run --entrypoint bash --privileged --network host --ipc host -v /:/host  \
-    --shm-size=1024g --ulimit memlock=-1 \
-    --name $CONTAINER_NAME \
-    "$DOCKER_IMAGE" -c "/host${CICD_DIR}/build.sh"
+env
